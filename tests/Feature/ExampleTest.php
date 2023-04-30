eba6978e31cfe974e3dcb6a348998744c7e7a942
Yuki Adachi <yuki777@gmail.com>
2023-04-30 16:20:28 +0900

    Add database test
    `php artisan migrate`
    `php artisan test`

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic test example.
      */
@@ -15,5 +18,16 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_database_users(): void
+    {
+        $users = DB::select('select * from users');
+        $this->assertSame(0, count($users));
+
+        DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['Marc', 'marc@example.com', 'password']);
+
+        $users = DB::select('select * from users');
+        $this->assertSame(1, count($users));
     }
 }
